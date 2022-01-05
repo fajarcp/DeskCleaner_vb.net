8f99868cb9b881bed3b59587ed0ae4ae9c0b2e28
fajarcp <56840070+fajarcp@users.noreply.github.com>
2022-01-05 11:51:40 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button1.FlatAppearance.BorderSize = 5
+        Me.Button1.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.Button1.Location = New System.Drawing.Point(177, 82)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(122, 46)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Tag = ""
+        Me.Button1.Text = "START"
+        Me.ToolTip1.SetToolTip(Me.Button1, "TO START DESKTOP CLEANING")
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label1.Location = New System.Drawing.Point(274, 170)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(134, 23)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = " BACKUP DATA"
+        Me.Label1.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label2.Location = New System.Drawing.Point(295, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 23)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = " STATUS"
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button2.FlatAppearance.BorderSize = 5
+        Me.Button2.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime
+        Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(177, 206)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(122, 46)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Tag = ""
+        Me.Button2.Text = "DELETE BACKUP DATA"
+        Me.ToolTip1.SetToolTip(Me.Button2, "TO DELETE BACKUP DATA")
+        Me.Button2.UseVisualStyleBackColor = False
+        Me.Button2.Visible = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Button3.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button3.FlatAppearance.BorderSize = 5
+        Me.Button3.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime
+        Me.Button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(377, 82)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(122, 46)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Tag = ""
+        Me.Button3.Text = "CANCEL"
+        Me.ToolTip1.SetToolTip(Me.Button3, "TO STOP DESKTOP CLEANING")
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Button4.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.Button4.FlatAppearance.BorderSize = 5
+        Me.Button4.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime
+        Me.Button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.ForeColor = System.Drawing.Color.White
+        Me.Button4.Location = New System.Drawing.Point(377, 206)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(122, 46)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "OPEN BACKUP FOLDER"
+        Me.ToolTip1.SetToolTip(Me.Button4, "TO OPEN BACKUP FOLDE")
+        Me.Button4.UseVisualStyleBackColor = False
+        Me.Button4.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 323)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Visible = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label4.Location = New System.Drawing.Point(24, 366)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 22)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Label4"
+        Me.Label4.Visible = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(174, 296)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(73, 108)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "."
+        Me.Label5.Visible = False
+        '
+        'ToolTip1
+        '
+        Me.ToolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Location = New System.Drawing.Point(499, 386)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(198, 26)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Created by:FAJAR,ATHUL and VISHNU" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "STM KANNUR CSE 2017"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(709, 421)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.ForeColor = System.Drawing.Color.DeepSkyBlue
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "DESKCLEAN"
+        Me.TransparencyKey = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,122 @@
+﻿Public Class Form1
+    Dim key, subk As Microsoft.Win32.RegistryKey
+    Dim strSystemDir As String = Environment.SystemDirectory.Substring(0, 2)
+    Dim AppPath As String = strSystemDir & "\DESKCLEAN\Backtask.exe"             'Needed for startup
+    Dim AutoStart As String = Environment.GetFolderPath(Environment.SpecialFolder.Startup) & "/" & IO.Path.GetFileName(AppPath)     'Needed for statrup
+    Dim HideFile As IO.FileInfo = My.Computer.FileSystem.GetFileInfo(Environment.GetFolderPath(Environment.SpecialFolder.Startup))   'Needed for startup
+    Dim LinkPath As String
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        key = My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE", True)
+        key.CreateSubKey("deskclean")
+        subk = My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE\deskclean\", True)
+        subk.SetValue("st", "start")
+        subk.Close()
+        key.Close()
+
+
+        Try
+            IO.File.Copy(AppPath, AutoStart)
+            HideFile.IsReadOnly = True                                      'Startup
+            HideFile.Attributes = HideFile.Attributes Or IO.FileAttributes.Hidden
+        Catch ex As Exception
+
+        End Try
+        Button3.Enabled = True
+        Button1.Enabled = False
+        Button2.Visible = True
+        Label1.Visible = True
+        Button4.Visible = True
+        Label4.Visible = True
+        Label4.Text = "Status:Started"
+        Label5.Visible = True
+        Label5.ForeColor = Color.Green
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If My.Computer.FileSystem.DirectoryExists(My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\Deskclean\backup") Then
+            If MsgBox("Operation can't be reversed.Do you want to continue?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                My.Computer.FileSystem.DeleteDirectory(My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\Deskclean\backup", FileIO.DeleteDirectoryOption.DeleteAllContents)
+            End If
+        Else
+            MsgBox("Backup directory doesn't exists", MsgBoxStyle.OkOnly)
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        key = My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE", True)
+        key.CreateSubKey("deskclean")
+        subk = My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE\deskclean\", True)
+        subk.SetValue("st", "stop")
+        subk.Close()
+        key.Close()
+        
+        Try
+            IO.File.Delete(AutoStart)
+            HideFile.IsReadOnly = True                                      'Startup
+            HideFile.Attributes = HideFile.Attributes Or IO.FileAttributes.Hidden
+        Catch ex As Exception
+
+        End Try
+        Button1.Enabled = True
+        Button3.Enabled = False
+        Button2.Visible = False
+        Label1.Visible = False
+        Button4.Visible = False
+        Label4.Visible = True
+        Label4.Text = "Status:Not started"
+        Label5.Visible = True
+        Label5.ForeColor = Color.Red
+    End Sub
+
+    Private Sub Form1_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        If Not IO.File.Exists(strSystemDir & "\DESKCLEAN\DESKCLEANER.exe") Then
+            key = My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE", True)
+            subk = My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE\deskclean\", True)
+            subk.SetValue("st", "stop")
+            subk.Close()
+            key.Close()
+            IO.File.Delete(AutoStart)
+            HideFile.IsReadOnly = True                                      'Startup
+            HideFile.Attributes = HideFile.Attributes Or IO.FileAttributes.Hidden
+        End If
+    End Sub
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        key = My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE", True)
+        subk = My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE\deskclean\", True)
+        If subk.GetValue("st") = "start" Then
+            Button3.Enabled = True
+            Button1.Enabled = False
+            Button2.Visible = True
+            Label1.Visible = True
+            Button4.Visible = True
+            Label4.Visible = True
+            Label4.Text = "Status:Started"
+            Label5.Visible = True
+            Label5.ForeColor = Color.Green
+        Else
+            Button1.Enabled = True
+            Button3.Enabled = False
+            Button2.Visible = False
+            Label1.Visible = False
+            Button4.Visible = False
+            Label4.Visible = True
+            Label4.Text = "Status:Not Started"
+            Label5.Visible = True
+            Label5.ForeColor = Color.Red
+        End If
+        subk.Close()
+        key.Close()
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If My.Computer.FileSystem.DirectoryExists(My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\Deskclean\backup") Then
+
+            Process.Start("explorer.exe", My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\Deskclean\backup")
+        Else
+            MsgBox("Backup directory doesn't exists", MsgBoxStyle.OkOnly)
+        End If
+    End Sub
+End Class
